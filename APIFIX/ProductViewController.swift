a2b851bb5fcfa8b6eec7a96397832c58f6b27505
Harsh Bindra <harshbindra@v1020-wn-196-209.campus-dynamic.uwaterloo.ca>
2016-09-17 23:30:36 -0400

    Create UI

---
+++ APIFIX/ProductViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ProductViewController.swift
+//  APIFIX
+//
+//  Created by Harsh Bindra on 2016-09-17.
+//  Copyright © 2016 Harsh Bindra. All rights reserved.
+//
+
+import UIKit
+
+var macbookSelected = false;
+var ipadSelected = false;
+var ipodNanoSelected = false;
+var priceOfProduct = 7;
+
+class ProductViewController: UIViewController {
+    @IBAction func selectIPod(sender: AnyObject) {
+        self.performSegueWithIdentifier("showCurrency", sender: self)
+        ipodNanoSelected = true;
+        priceOfProduct = 250;
+    }
+    @IBAction func selectIPad(sender: AnyObject) {
+        self.performSegueWithIdentifier("showCurrency", sender: self)
+        ipadSelected = true;
+        priceOfProduct = 500;
+    }
+    @IBAction func selectMacbook(sender: AnyObject) {
+        
+        self.performSegueWithIdentifier("showCurrency", sender: self)
+        macbookSelected = true;
+        priceOfProduct = 1600;
+        
+    }
+
+}
